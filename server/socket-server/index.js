36300c8f58c00e522f5334a2612e4108cb819252
Marty McLaughlin <martymclaugh@gmail.com>
2017-12-03 17:08:31 -0800

    when new user joins, send last 50 messages

--- server/socket-server/index.js
+++ server/socket-server/index.js
@@ -4,17 +4,36 @@ import { socketEventTypes } from './socket-event-types';
 export default function (server) {
   const socketServer = io(server);
   const connections = [];
+  const userNames = ['marty'];
+  const lobbyMessages = [];
   var userId = 0;
 
   socketServer.on('connection', socket => {
     connections.push(socket);
     userId += 1;
-    console.log('here', userId)
 
     Object.keys(socketEventTypes).map(action => {
       socket.on(action, data => {
         socket.broadcast.emit(socketEventTypes[action], data);
       });
+    });
+
+    socket.on('createUsername', data => {
+      if (userNames.includes(data.username)) {
+        socket.emit('receiveUsernameTaken', data.username);
+      } else {
+        userNames.push(data.username);
+        socket.emit('receiveUsernameSuccess', {
+          username: data.username,
+          id: userId,
+        });
+        const lastFiftyLobbyMessages = lobbyMessages.slice(lobbyMessages.length - 50);
+        socket.emit('receiveLobbyMessage', lastFiftyLobbyMessages);
+      }
+    });
+    socket.on('sendLobbyMessage', data => {
+      lobbyMessages.push(data);
+      socket.broadcast.emit('receiveLobbyMessage', data);
     });
 
     socket.on('disconnect', () => {
