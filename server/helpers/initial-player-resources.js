1fa82f67d27de89d24687ce1582ce00a639195a7
Marty McLaughlin <marty@fandor.com>
2018-03-18 12:17:31 -0700

    add initial resources state

---
+++ server/helpers/initial-player-resources.js
@@ -0,0 +1,12 @@
+export default {
+  colonists: 0,
+  soylent: 0,
+  credits: 0,
+  distributedResources: {
+    nanoTubes: 0,
+    unobtanium: 0,
+    energyPlankton: 0,
+    giantSpiderSilk: 0,
+    rifles: 0,
+  },
+};
